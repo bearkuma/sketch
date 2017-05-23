a143595dc59a64e8903f73555aa54af6a4453efe
bearkuma <chatnodejs@yahoo.co.jp>
2017-05-24 03:05:16 +0900

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var server = http.createServer(function(req,res){
 			res.writeHead(200,{'Content-type':'text/html'});
 			var strm = fs.createReadStream(fullpath);
 			strm.pipe(res);
-		} else if (ext.match(/\.(png|jpg|jpeg|gif||css|js)$/) && x.pathname != '/chatserver.js'){
+		} else if (ext.match(/\.(png|jpg|jpeg|gif||css|js)$/) && x.pathname != '/server.js'){
 			var strm = fs.createReadStream(fullpath);
 			strm.pipe(res);
 		} else {
